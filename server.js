6f3531a8319079e062bc128337eb2cad65ea1e89
echinna31 <epradeepep@gmail.com>
2017-09-08 19:37:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 let app = express();
 const port = process.env.PORT || 3030;
+console.log(__dirname);
 app.use(express.static(__dirname));
 app.listen(port, function(){
     console.log("express server is up on port"+port);
